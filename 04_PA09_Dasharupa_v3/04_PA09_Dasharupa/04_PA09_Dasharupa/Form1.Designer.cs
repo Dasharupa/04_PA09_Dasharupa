d9e26f32b3e3c8a3a3ffc4b30112c8a317a4b765
h3x1s <64124310+h3x1s@users.noreply.github.com>
2020-06-16 00:52:00 +0800

    Updated PA09
    Updated PA09 by Riduan

--- 04_PA09_Dasharupa_v3/04_PA09_Dasharupa/04_PA09_Dasharupa/Form1.Designer.cs
+++ 04_PA09_Dasharupa_v3/04_PA09_Dasharupa/04_PA09_Dasharupa/Form1.Designer.cs
@@ -34,8 +34,8 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.rb_USDollar = new System.Windows.Forms.RadioButton();
+            this.rb_JPYen = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.rb_Ringgit = new System.Windows.Forms.RadioButton();
@@ -43,10 +43,10 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(216, 80);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Location = new System.Drawing.Point(288, 98);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.Size = new System.Drawing.Size(197, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -58,20 +58,19 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(216, 245);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Location = new System.Drawing.Point(288, 302);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(149, 20);
+            this.textBox2.Size = new System.Drawing.Size(197, 22);
             this.textBox2.TabIndex = 2;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(136, 83);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(181, 102);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.Size = new System.Drawing.Size(64, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Amount :";
             this.label1.Click += new System.EventHandler(this.Label1_Click);
@@ -79,45 +78,44 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(106, 248);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(141, 305);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.Size = new System.Drawing.Size(133, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Converted Amount :";
             // 
-            // radioButton1
+            // rb_USDollar
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(242, 119);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(70, 17);
-            this.radioButton1.TabIndex = 5;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "US Dollar";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            this.rb_USDollar.AutoSize = true;
+            this.rb_USDollar.Location = new System.Drawing.Point(323, 146);
+            this.rb_USDollar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rb_USDollar.Name = "rb_USDollar";
+            this.rb_USDollar.Size = new System.Drawing.Size(89, 21);
+            this.rb_USDollar.TabIndex = 5;
+            this.rb_USDollar.TabStop = true;
+            this.rb_USDollar.Text = "US Dollar";
+            this.rb_USDollar.UseVisualStyleBackColor = true;
+            this.rb_USDollar.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
             // 
-            // radioButton2
+            // rb_JPYen
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(242, 158);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(96, 17);
-            this.radioButton2.TabIndex = 6;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Japanese YEN";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            this.rb_JPYen.AutoSize = true;
+            this.rb_JPYen.Location = new System.Drawing.Point(323, 194);
+            this.rb_JPYen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rb_JPYen.Name = "rb_JPYen";
+            this.rb_JPYen.Size = new System.Drawing.Size(123, 21);
+            this.rb_JPYen.TabIndex = 6;
+            this.rb_JPYen.TabStop = true;
+            this.rb_JPYen.Text = "Japanese YEN";
+            this.rb_JPYen.UseVisualStyleBackColor = true;
+            this.rb_JPYen.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(457, 166);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(609, 204);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 32);
+            this.button1.Size = new System.Drawing.Size(115, 39);
             this.button1.TabIndex = 7;
             this.button1.Text = "Convert";
             this.button1.UseVisualStyleBackColor = true;
@@ -125,10 +123,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(457, 233);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(609, 287);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(86, 30);
+            this.button2.Size = new System.Drawing.Size(115, 37);
             this.button2.TabIndex = 8;
             this.button2.Text = "Clear";
             this.button2.UseVisualStyleBackColor = true;
@@ -137,9 +135,10 @@
             // rb_Ringgit
             // 
             this.rb_Ringgit.AutoSize = true;
-            this.rb_Ringgit.Location = new System.Drawing.Point(242, 196);
+            this.rb_Ringgit.Location = new System.Drawing.Point(323, 241);
+            this.rb_Ringgit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rb_Ringgit.Name = "rb_Ringgit";
-            this.rb_Ringgit.Size = new System.Drawing.Size(108, 17);
+            this.rb_Ringgit.Size = new System.Drawing.Size(140, 21);
             this.rb_Ringgit.TabIndex = 9;
             this.rb_Ringgit.TabStop = true;
             this.rb_Ringgit.Text = "Malaysian Ringgit";
@@ -148,21 +147,21 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.rb_Ringgit);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.rb_JPYen);
+            this.Controls.Add(this.rb_USDollar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
-            this.Text = "25_Riduan_PA09";
+            this.Text = "04_PA09_Dasharupa";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -176,8 +175,8 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton rb_USDollar;
+        private System.Windows.Forms.RadioButton rb_JPYen;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RadioButton rb_Ringgit;
